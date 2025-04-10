437e57bf03bf03b5a97dcaa2c66a39847e977ac6
pauldjoplin <pdjnetwork95@gmail.com>
2025-04-10 11:12:29 -0500

    Input validation for numerical values 1-4 added, next is to add validation for if the string entered matches any of the produce in the list.

--- Project3/IOControls.cpp
+++ Project3/IOControls.cpp
@@ -2,17 +2,19 @@
 
 #include <iostream>
 
-
-void IOControls::displayMenu(const map<string, int>& mapData){
+void IOControls::displayMenu(const map<string, int>& mapData) {
 	
-	cout << "File uploaded...choose a menu option to continue." << endl;
+	cout << "File uploaded..backup created...choose a menu option to continue.\n";
 
 	cout << "1: Check the frequency of a product via word search.\n" <<
 		"2: Print a list of items and their purchase frequency.\n" <<
 		"3: Print a histogram of items and their purchase frequency.\n" <<
-		"4: Exit the program.\n";
+		"4: Exit the program.\n"
+		"Enter the menu option (1-4): ";
+	
+	do {
+		menuChoice = (GetValidatedInput<int>(""));
 
-
-
+	} while (menuChoice != 4);
 }
 
--- Project3/IOControls.h
+++ Project3/IOControls.h
@@ -4,6 +4,8 @@
 #include <map>
 #include <string>
 #include <iostream>
+#include <sstream>
+#include <stdexcept>
 
 using namespace std;
 
@@ -14,7 +16,47 @@ class IOControls
 public:
 
 	void displayMenu(const map<string, int>& mapData);
+
+	template<typename T>
+	T GetValidatedInput(const string&);
+
 	int menuChoice;
+	string produceChoice;
 
 };
+
+template<typename T>
+T IOControls::GetValidatedInput(const string&) {
+	T value;
+	string input;
+
+	bool valid;
+
+	do {
+		valid = true;
+
+		try {
+			getline(cin, input);
+
+			if (input.empty()) {
+				throw runtime_error("Input cannot be blank. Please enter a value.");
+			}
+
+			stringstream ss(input);
+			ss >> value;
+
+			if (ss.fail() || value <= 0 || value > 4 || !ss.eof()) {
+				throw runtime_error("Invalid input, please enter (1-4).");
+			}
+			
+		}
+		catch (const runtime_error& e) {
+			cerr << "Error: " << e.what() << endl; 
+			valid = false;
+		}
+	} while (!valid);
+
+	return value;
+}
+
 #endif
